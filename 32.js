38aa517e472273293561248437096a86a907e546
Emily Vazquez <a357623@uach.mx>
2024-05-02 12:13:41 -0600

    Crear un arreglo de 20 elementos con nombres de personas. Mostrar los elementos de la lista debiendo ir cada uno en una fila distinta.

---
+++ 32.js
@@ -0,0 +1,3 @@
+const nombres = ["Ana", "Beatriz", "Carlos", "David", "Elena", "Fernando", "Gabriela", "Hector", "Irene", "Juan", "Karen", "Luis", "Marta", "Nora", "Oscar", "Patricia", "Quintin", "Rosa", "Sofia", "Tomás"];
+nombres.forEach(nombre => console.log(nombre));
+
